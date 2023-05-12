07ce1784a505d86e9812fcd070a6ba447d162c7e
phsu40 <phsu40@gatech.edu>
2023-05-12 16:28:48 -0700

    Implement header component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Fragment } from 'react';
 import Counter from './components/Counter';
+import Header from './components/Header';
 
 
 function App() {
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+
+      <Counter />
+    </Fragment>
   );
 }
 
